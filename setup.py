e46608f0de625396400c3476bb96383564f69e93
Hamza Tariq <hamzatrq116@gmail.com>
2024-04-12 11:11:46 +0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,77 @@
+import re
+import os
+import codecs
+import pathlib
+from os import path
+from io import open
+from setuptools import setup, find_packages
+from pkg_resources import parse_requirements
+
+
+def read_requirements(path):
+    with open(path, "r") as f:
+        requirements = f.read().splitlines()
+        processed_requirements = []
+
+        for req in requirements:
+            # For git or other VCS links
+            if req.startswith("git+") or "@" in req:
+                pkg_name = re.search(r"(#egg=)([\w\-_]+)", req)
+                if pkg_name:
+                    processed_requirements.append(pkg_name.group(2))
+                else:
+                    # You may decide to raise an exception here,
+                    # if you want to ensure every VCS link has an #egg=<package_name> at the end
+                    continue
+            else:
+                processed_requirements.append(req)
+        return processed_requirements
+
+
+requirements = read_requirements("requirements.txt")
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+# loading version from setup.py
+# TODO: Change the template folder to the name of the package
+with codecs.open(
+    os.path.join(here, "template/__init__.py"), encoding="utf-8"
+) as init_file:
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", init_file.read(), re.M
+    )
+    version_string = version_match.group(1) # type: ignore
+
+setup(
+    name="hip_subnet",
+    version=version_string,
+    description="Human in the middle subnet for bittensor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HIP-Labs/bittensor-hip-subnet",
+    author="Hamza Tariq",
+    packages=find_packages(),
+    include_package_data=True,
+    author_email="hamzatrq116@gmail.com",
+    license="MIT",
+    python_requires=">=3.8",
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
