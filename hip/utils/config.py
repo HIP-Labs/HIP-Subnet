fa02b0bbedea055103ecfaba95fe5e1629878257
Hamza Tariq <hamzatrq116@gmail.com>
2024-04-24 18:14:23 +0500

    Adding timeout

--- hip/utils/config.py
+++ hip/utils/config.py
@@ -179,7 +179,7 @@ def add_validator_args(cls, parser):
         "--neuron.timeout",
         type=float,
         help="The timeout for each forward call in seconds.",
-        default=60,
+        default=180,
     )
 
     parser.add_argument(
--- hip/validator/forward.py
+++ hip/validator/forward.py
@@ -59,6 +59,7 @@ async def forward(self):
         # Send the query to selected miner axons in the network.
         axons=[self.metagraph.axons[uid] for uid in miner_uids],
         synapse=task,
+        timeout=self.config.neuron.timeout,
         # All responses have the deserialize function called on them before returning.
         # You are encouraged to define your own deserialization function.
         deserialize=True,
