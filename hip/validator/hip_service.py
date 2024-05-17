2bceb23ec57c7171e0613acfe6a7418d35ff79e4
Hamza Tariq <hamzatrq116@gmail.com>
2024-05-17 18:18:24 +0500

    logging

--- hip/validator/hip_service.py
+++ hip/validator/hip_service.py
@@ -2,13 +2,18 @@ from hip.protocol import TaskSynapse
 import hip.validator.generator
 import random
 import uuid
+import bittensor as bt
 
 
 async def get_llm_task() -> TaskSynapse:
+    bt.logging.info("Generating a new task")
     context = hip.validator.generator.generate_paragraph()
+    bt.logging.info(f"Context: {context}")
     taskType = random.choice(["qa", "sentiment_analysis", "summarization"])
+    bt.logging.info(f"Task Type: {taskType}")
     if taskType == "qa":
         task = hip.validator.generator.generate_question_answer(context)
+        bt.logging.info(f"Task: {task}")
         return TaskSynapse(
             id=str(uuid.uuid4()),
             label=task["question"],
@@ -20,6 +25,7 @@ async def get_llm_task() -> TaskSynapse:
         )
     elif taskType == "sentiment_analysis":
         sentiment = hip.validator.generator.get_sentiment(context)
+        bt.logging.info(f"Task: {sentiment}")
         return TaskSynapse(
             id=str(uuid.uuid4()),
             label="What is the sentiment of the given text?",
@@ -32,6 +38,7 @@ async def get_llm_task() -> TaskSynapse:
     elif taskType == "summarization":
         summaries = hip.validator.generator.generate_summaries(context)
         shuffledSummaries = summaries.copy()
+        bt.logging.info(f"Task: {summaries}")
         random.shuffle(shuffledSummaries)
 
         return TaskSynapse(
