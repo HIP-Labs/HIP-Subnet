da00ba03cc6630c59c1fc9059e808c26fcff3b53
Hamza Tariq <hamzatrq116@gmail.com>
2024-05-17 06:14:59 +0500

    llm based generation of tasks

--- hip/utils/hip_service.py
+++ hip/utils/hip_service.py
@@ -1,19 +1,48 @@
-import aiohttp
 from hip.protocol import TaskSynapse
-from hip.constants import HIP_SERVICE_URL
+import hip.validator.generator
+import random
+import uuid
 
 
-async def get_task() -> TaskSynapse:
-    # send a GET request to the HIP_SERVICE_URL and create a TaskSynapse object
-    async with aiohttp.ClientSession() as session:
-        async with session.get(HIP_SERVICE_URL) as response:
-            response_data = await response.json()
-            return TaskSynapse(
-                id=response_data["id"],
-                label=response_data["label"],
-                type=response_data["type"],
-                options=response_data["options"],
-                value=response_data["value"],
-                image=response_data["image"],
-                answer="",
-            )
+async def get_llm_task() -> TaskSynapse:
+    context = hip.validator.generator.generate_paragraph()
+    taskType = random.choice(["qa", "sentiment_analysis", "summarization"])
+    if taskType == "qa":
+        task = hip.validator.generator.generate_question_answer(context)
+        return TaskSynapse(
+            id=str(uuid.uuid4()),
+            label=task["question"],
+            type="select",
+            options=task["options"],
+            value=context,
+            image="",
+            answer=task["options"][task["answer"]],
+        )
+    elif taskType == "sentiment_analysis":
+        sentiment = hip.validator.generator.get_sentiment(context)
+        return TaskSynapse(
+            id=str(uuid.uuid4()),
+            label="What is the sentiment of the given text?",
+            type="select",
+            options=["Positive", "Negative", "Neutral"],
+            value=context,
+            image="",
+            answer=sentiment,
+        )
+    elif taskType == "summarization":
+        summaries = hip.validator.generator.generate_summaries(context)
+        shuffledSummaries = summaries.copy()
+        random.shuffle(shuffledSummaries)
+
+        return TaskSynapse(
+            id=str(uuid.uuid4()),
+            label="Select the correct summary for the given text:",
+            type="select",
+            options=shuffledSummaries,
+            value=context,
+            image="",
+            answer=summaries[0],
+        )
+    else:
+        # throw an error
+        raise ValueError("Invalid task type")
