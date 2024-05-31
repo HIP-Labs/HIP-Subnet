66d5328ed88543239f22e526a560918619c36b75
Hamza Tariq <hamzatrq116@gmail.com>
2024-06-01 01:00:33 +0500

    typo fix

--- scripts/setup_validator.sh
+++ scripts/setup_validator.sh
@@ -87,7 +87,7 @@ setup_gpu() {
 setup_python() {
     echo "Installing Python 3.10"
     sudo add-apt-repository ppa:deadsnakes/ppa -y
-    sudo-E apt update
+    sudo -E apt update
     sudo -E apt install -y python3.10 python3.10-venv python3.10-dev
     sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
 }
