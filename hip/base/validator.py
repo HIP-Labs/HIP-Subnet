247a932d92cf2db01bb6979f11f162572d15b55f
Hamza Tariq <hamzatrq116@gmail.com>
2024-06-16 04:15:21 +0500

    Moving update scores to base class

--- hip/base/validator.py
+++ hip/base/validator.py
@@ -24,13 +24,14 @@ import asyncio
 import argparse
 import threading
 import bittensor as bt
-
+import time
 from typing import List
 from traceback import print_exception
 
 from hip.base.neuron import BaseNeuron
 from hip.mock import MockDendrite
 from hip.utils.config import add_validator_args
+from hip.validator.reward import linear_rewards
 
 
 class BaseValidatorNeuron(BaseNeuron):
@@ -322,8 +323,68 @@ class BaseValidatorNeuron(BaseNeuron):
     def update_scores(
         self, rewards: torch.FloatTensor, uids: List[int], question_type: str
     ):
-        """This function should be overridden by the subclass."""
-        bt.logging.error("update_scores called from base file")
+        current_time = time.time()
+
+        for uid, reward in zip(uids, rewards):
+            if uid not in self.rewards_log:
+                self.rewards_log[uid] = []
+            self.rewards_log[uid].append((reward.item(), current_time, question_type))
+
+        for uid in uids:
+            # Filter rewards for the last 24 hours
+            recent_rewards = [
+                (r, t, qtype)
+                for r, t, qtype in self.rewards_log[uid]
+                if current_time - t <= 86400
+            ]
+
+            # Separate regular questions and captchas
+            correct_answers = sum(1 for r, t, qtype in recent_rewards if r > 0.0)
+            correct_capthas = sum(
+                1 for r, t, qtype in recent_rewards if r > 0.0 and qtype == "captcha"
+            )
+
+            captcha_penalties = sum(
+                1 for r, t, qtype in recent_rewards if qtype == "captcha" and r == 0.0
+            )
+            wrong_answers = sum(
+                1 for r, t, qtype in recent_rewards if r == 0.0 and qtype != "captcha"
+            )
+            score = linear_rewards(self, correct_answers)
+
+            penalize_score = True
+            # Realistically, a real human being can't answer for more than 8 hours a day
+            # so we do not penalize the score if the miner has answered more than 8 hours a day
+            # We know that probability of a captcha occuring is 10% in whole day we will get
+            # 10% of 480 = 48 captchas in a day
+            # in 8 hours we will get 48/3 = 16 captchas
+            if correct_capthas > 16:
+                penalize_score = False
+
+            if penalize_score:
+                # For each wrong answer, penalize the score by 1% by computing the penalty
+                # as 1% of the total number of wrong answers
+                for i in range(wrong_answers):
+                    score = score * 0.99
+
+                # For each wrong captcha answer, penalize the score by 5% by computing the penalty
+                # as 5% of the total number of failed captchas
+                for i in range(captcha_penalties):
+                    score = score * 0.95
+            if score > 0.0:
+                bt.logging.info(
+                    f"Updating score for miner {uid} with score {score} and correct answers {correct_answers} and penalize_score {penalize_score}"
+                )
+            # Update the scores tensor
+            self.scores[uid] = torch.FloatTensor([score])
+
+        # Remove old records to free up memory
+        for uid in list(self.rewards_log.keys()):
+            self.rewards_log[uid] = [
+                (r, t, qtype)
+                for r, t, qtype in self.rewards_log[uid]
+                if current_time - t <= 86400
+            ]
 
     def save_state(self):
         """Saves the state of the validator to a file."""
