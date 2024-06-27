9a4ded6ec4ecc3b48cfa85be3a17230d187a61dc
Hamza Tariq <hamzatrq116@gmail.com>
2024-06-27 17:18:11 +0500

    adding new miner configuration script

--- scripts/setup_miner.sh
+++ scripts/setup_miner.sh
@@ -1,181 +1,185 @@
 #!/bin/bash
 
-# Set noninteractive mode
 export DEBIAN_FRONTEND=noninteractive
 export NEEDRESTART_MODE=a
 export NEEDRESTART_SUSPEND=1
 
-export BIYellow='\033[1;93m'
+#    ___ ___  _  _ ___ _____ _   _  _ _____ ___
+#   / __/ _ \| \| / __|_   _/_\ | \| |_   _/ __|
+#  | (_| (_) | .` \__ \ | |/ _ \| .` | | | \__ \
+#   \___\___/|_|\_|___/ |_/_/ \_\_|\_| |_| |___/
+#
+NODE_VERSION="20.14.0"
+PYTHON_VERSION="python3.10"
+GITHUB_REPO_URL="https://github.com/HIP-Labs/HIP-Subnet"
 
-# Function to check if a command exists
-command_exists() {
-    command -v "$1" >/dev/null 2>&1
+#   __  __ ___ _____ _  _  ___  ___  ___
+#  |  \/  | __|_   _| || |/ _ \|   \/ __|
+#  | |\/| | _|  | | | __ | (_) | |) \__ \
+#  |_|  |_|___| |_| |_||_|\___/|___/|___/
+
+stop_all_services() {
+    # Stop the frontend service if it is running or installed
+    if sudo systemctl is-active --quiet uvicorn_frontend; then
+        sudo systemctl stop uvicorn_frontend
+        # now disable the service
+        sudo systemctl disable uvicorn_frontend
+    fi
+    # there is a chance that nvm is not configured in the current shell
+    if [ -f ~/.nvm/nvm.sh ]; then
+        source ~/.nvm/nvm.sh
+    fi
+    if command_exists pm2; then
+        pm2 stop all
+        pm2 unstartup
+        pm2 delete all
+    fi
 }
 
-check_mnemonic() {
-    if [ -z "$MNEMONIC" ]; then
-        echo "MNEMONIC variable is not set. Exiting"
-        echo "Please set the MNEMONIC variable to the mnemonic of the wallet"
-        echo "Example: export MNEMONIC='word1 word2 word3 ...'"
+check_supported_os() {
+    # Check if the operating system is Ubuntu
+    if [[ $(lsb_release -si) != "Ubuntu" ]]; then
+        echo "Operating system not supported"
         exit 1
     fi
+
+    # Check the Ubuntu version
+    VERSION=$(lsb_release -sr)
+    if [[ $VERSION != "22.04" ]]; then
+        echo "OS version not supported"
+        exit 1
+    fi
+
+    # Continue with the rest of the script if both conditions are met
+    echo "Operating system and version are supported"
 }
 
-# Function to install Docker
-install_docker() {
-    if ! command_exists docker; then
-        echo "Installing Docker"
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-        sudo -E apt update
-        sudo -E apt install docker-ce -y
-        sudo systemctl start docker
-        sudo systemctl enable docker
-        sudo usermod -aG docker "$USER"
+install_python() {
+    if ! command_exists $PYTHON_VERSION; then
+        echo "Installing Python 3.10"
+        sudo add-apt-repository ppa:deadsnakes/ppa -y
+        sudo apt update
+        sudo apt install -y $PYTHON_VERSION $PYTHON_VERSION-venv $PYTHON_VERSION-dev python3-pip
+        sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+        if [ -d "$HOME/.local/bin" ]; then
+            echo "export PATH=$HOME/.local/bin:$PATH" >>~/.bashrc
+            source ~/.bashrc
+        fi
     else
-        echo "Docker is already installed"
+        echo "Python 3.10 is already installed"
     fi
 }
 
-# Function to install Node.js and PM2
 install_node_pm2() {
+    source ~/.bashrc
     if ! command_exists nvm; then
         echo "Installing Node.js and PM2"
         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
         source ~/.nvm/nvm.sh
-        nvm install --lts
+        nvm install $NODE_VERSION
+        nvm use $NODE_VERSION
+        nvm alias default $NODE_VERSION
         npm install -g pm2
+        pm2 install pm2-logrotate
+        pm2 startup
+        pm2 save
     else
         echo "NVM is already installed"
+        nvm install $NODE_VERSION
+        nvm use $NODE_VERSION
+        nvm alias default $NODE_VERSION
+        npm install -g pm2
+        pm2 install pm2-logrotate
+        pm2 startup
+        pm2 save
     fi
 }
 
-# Function to clone and set up HIP Subnet
-setup_hip_subnet() {
-    echo "Setting up HIP Subnet"
-    cd ~ || exit
-    if [ ! -d "HIP-Subnet" ]; then
-        git clone https://github.com/HIP-Labs/HIP-Subnet
+setup_subnet() {
+    # check if git is installed
+    if ! command_exists git; then
+        sudo apt install -y git
+    fi
+    if [ -d ~/HIP-Subnet ]; then
+        rm -rf ~/HIP-Subnet
+    fi
+    git clone $GITHUB_REPO_URL ~/HIP-Subnet
+}
+
+install_requirements() {
+    if [ -f ~/HIP-Subnet/requirements.txt ]; then
+        cd ~/HIP-Subnet &&
+            pip install -r requirements.txt &&
+            pip install -e .
     else
-        cd HIP-Subnet || exit
-        git reset --hard
-        git checkout main
-        git pull
-        cd ..
+        echo "requirements.txt not found"
     fi
 }
 
-# Function to install Python 3.10 and set up virtual environment
-setup_python() {
-    echo "Installing Python 3.10"
-    sudo add-apt-repository ppa:deadsnakes/ppa -y
-    sudo-E apt update
-    sudo -E apt install -y python3.10 python3.10-venv python3.10-dev
-    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
-}
+setup_wallet() {
+    echo "Creating wallet"
+    if [ -d ~/.bittensor/wallets ]; then
+        echo "Wallet already exists"
 
-# Define setup_wallet() function
-setup_miner() {
-    if ! command_exists btcli; then
-        echo "Btcli not found. Exiting"
-        exit 1
     else
-        # if MNEMONIC variable is not set, throw an error
-        check_mnemonic
-        btcli wallet regen_coldkey --wallet.name default --wallet.hotkey default --subtensor.network test --no_password --no_prompt --mnemonic $MNEMONIC
-        btcli wallet regen_hotkey --wallet.name default --wallet.hotkey default --subtensor.network test --no_password --no_prompt --mnemonic $MNEMONIC
-        # btcli subnet register --wallet.name default --wallet.hotkey default --subtensor.network test --no_prompt --netuid 134
+        btcli w new_coldkey --wallet.name default --no_password --no_prompt &&
+            btcli w new_hotkey --wallet.name default --wallet.hotkey default --no_password --no_prompt
     fi
 }
 
-# Define setup_frontend() function
 setup_frontend() {
-    # Define the service unit file path
-    SERVICE_FILE="/etc/systemd/system/uvicorn_frontend.service"
-    # Check if the service unit file already exists
-    if [ -f "$SERVICE_FILE" ]; then
-        echo "Service unit file already exists"
-        # restart the service
-        sudo systemctl restart uvicorn_frontend
-        return
+    if [ -f /etc/nginx/sites-enabled/reverse-proxy ]; then
+        echo "frontend already setup"
+        pm2 stop frontend
+        pm2 delete frontend
+        pm2 start uvicorn --name frontend --interpreter python -- frontend:app --host 0.0.0.0 --port 5001
+    else
+        cd ~/HIP-Subnet/ &&
+            pm2 start uvicorn --name frontend --interpreter python -- frontend:app --host 0.0.0.0 --port 5001 &&
+            sudo apt install -y nginx &&
+            sudo bash -c 'cat <<EOF > /etc/nginx/sites-available/reverse-proxy
+server {
+    listen 80;
+
+    location / {
+        proxy_pass http://127.0.0.1:5001;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+}
+EOF'
+        sudo rm /etc/nginx/sites-enabled/default
+        # Enable the reverse proxy configuration
+        sudo ln -s /etc/nginx/sites-available/reverse-proxy /etc/nginx/sites-enabled/
+        sudo nginx -t
+        # If the test passes, reload Nginx to apply changes
+        if [ $? -eq 0 ]; then
+            sudo systemctl reload nginx
+            echo "Nginx configuration reloaded successfully."
+        else
+            echo "Nginx configuration test failed. Please check the configuration and try again."
+        fi
     fi
-    echo "Setting up frontend service"
-    # Define the content of the service unit file
-    SERVICE_CONTENT="[Unit]
-Description=UVicorn Frontend Service
-After=network.target
-
-[Service]
-User=ubuntu
-Group=ubuntu
-WorkingDirectory=/home/ubuntu/HIP-Subnet
-ExecStart=/home/ubuntu/HIP-Subnet/venv/bin/uvicorn frontend:app --host 0.0.0.0 --port 5001
-Restart=always
-
-[Install]
-WantedBy=multi-user.target"
-
-    # Write the service unit file
-    echo "$SERVICE_CONTENT" | sudo tee "$SERVICE_FILE" >/dev/null
-
-    # Reload systemd
-    sudo systemctl daemon-reload
-
-    # Enable the service
-    sudo systemctl enable uvicorn_frontend
-
-    # Start the service
-    sudo systemctl start uvicorn_frontend
-
-    # Check the status of the service
-    sudo systemctl status uvicorn_frontend˝
 }
 
-# Main script
-check_mnemonic
-echo "Setting up Subtensor Testnet"
-echo "Installing dependencies"
-sudo -E apt update
-sudo -E apt install -y apt-transport-https ca-certificates curl software-properties-common tmux
+#   __  __   _   ___ _  _   ___  ___ ___ ___ ___ _____
+#  |  \/  | /_\ |_ _| \| | / __|/ __| _ \_ _| _ \_   _|
+#  | |\/| |/ _ \ | || .` | \__ \ (__|   /| ||  _/ | |
+#  |_|  |_/_/ \_\___|_|\_| |___/\___|_|_\___|_|   |_|
+#
 
-install_docker
-
-# Make sure the user is in the docker group
-docker compose down --volumes
-
+sudo apt update
 install_node_pm2
+install_python
+setup_subnet
+install_requirements
 
-echo "Running Subtensor Testnet in Docker"
-cd ~ || exit
-
-git clone https://github.com/opentensor/subtensor.git
-cd subtensor || exit
-git checkout main
-sudo ./scripts/run/subtensor.sh -e docker --network testnet --node-type lite
-setup_hip_subnet
-
-cd ~/HIP-Subnet || exit
-setup_python
-
-echo "Creating virtual environment and installing HIP Subnet"
-python3.10 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-pip install -e .
-
-setup_miner
+setup_wallet
 setup_frontend
 
-echo "HIP Subnet setup complete"
-echo "\n\n"
-echo "Note:"
-echo "Run the following command to setup the python env:"
-echo "$ cd ~/HIP-Subnet && source venv/bin/activate"
-echo "Now ensure that your coldkey has enough Tao to register a miner."
-echo "Run the following command to check your balance:"
-echo "$ btcli w balance --wallet.name default --subtensor.network test --wallet.path ~/.bittensor/wallets/ --no_prompt"
-echo "Run the following command to register to the subnet:"
-echo "$ btcli subnet register --wallet.name default --wallet.hotkey default --subtensor.network test --no_prompt --netuid 134"
-echo "Run the following command to start the server:"
-echo "$ source ~/.nvm/nvm.sh && pm2 start ecosystem.config.js"
-echo "Now open the browser and go to http://[server-public-ip]:3001 to see the HIP Subnet dashboard"
+echo "RUN:"
+echo "btcli subnet register --netuid 36  --wallet.name default --wallet.hotkey default --no_prompt"
+echo "cd ~/HIP-Subnet && pm2 start neurons/miner.py --name miner --interpreter python --  --netuid 36  --wallet.name default --wallet.hotkey default && pm2 save"
