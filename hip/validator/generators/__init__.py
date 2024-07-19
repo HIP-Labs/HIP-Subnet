400493ff48b95326adae8319783e6268315ac543
Hamza Tariq <hamzatrq116@gmail.com>
2024-07-19 05:44:00 +0500

    adding generation import

--- hip/validator/generators/__init__.py
+++ hip/validator/generators/__init__.py
@@ -1,10 +1,12 @@
 from .captcha_generator import generate_captcha_task
 from .image_generator import generate_image_task
 from .llm_generator import generate_llm_task
+from .math_generator import generate_math_task
 
 # exporting as generator
 __all__ = [
     "generate_captcha_task",
     "generate_image_task",
     "generate_llm_task",
+    "generate_math_task",
 ]
